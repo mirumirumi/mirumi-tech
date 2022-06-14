d11a879fc0978753bbaab0bbccae91311200a607
mirumi <mail@mirumi.me>
2022-06-14 23:44:56 +0900

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const router = useRouter()  // same for navigateTo()
+
+definePageMeta({
+  title: "",
+  // middleware: ["navigation-guards"],
+})
+
+
+
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
