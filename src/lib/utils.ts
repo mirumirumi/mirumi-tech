9fd4b034ceb64f699873b2954df830b10467af4e
mirumi <info@milmemo.net>
2022-06-19 11:09:29 +0900

    add: today()

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
+export const today = () => {
+  return `${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()}`
+}
+
 export const round = (value: number, base = 6): number => {
   return Math.round(value * (10**base)) / (10**base)
 }
