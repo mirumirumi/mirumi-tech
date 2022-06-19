ef648dc0bb8e7cbd9666c19c0ccd35a867d38c8f
mirumi <info@milmemo.net>
2022-06-19 22:49:59 +0900

    add: meta tags in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,17 +5,36 @@ export default defineNuxtConfig({
     baseURL: "/",  // can override by NUXT_APP_BASE_URL
     buildAssetsDir: "_nuxt",
     head: {
-      "charset": "utf-8",
-      "meta": [
+      charset: "utf-8",
+      meta: [
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
         { name: "sign", content: "880f27e6d0c4daf6c6143beb568a73075f02b75d53e690e6bb79297435d7cf5a8404a4c84ba35416556b92628987d76b9067a4a4ef5d30f63b2b7d0f89b0a26b" },
+        { name: "robots", content: "max-image-preview:large" },
+        { name: "description", content: "みるみの技術ブログ" },
+        { name: "keywords", content: "みるめも,みるみ,blog,technology,programming" },
+        { name: "thumbnail", content: "/main-visual.png" },
+        { property: "og:type", content: "website" },
+        { property: "og:description", content: "みるみの技術ブログ" },
+        { property: "og:title", content: "mirumi.tech" },
+        { property: "og:url", content: "https://mirumi.tech" },
+        { property: "og:image", content: "/main-visual.png" },
+        { property: "og:site_name", content: "mirumi.tech" },
+        { property: "og:locale", content: "ja_JP" },
+        { property: "og:app_id", content: "" },
+        { property: "twitter:title", content: "mirumi.tech" },
+        { property: "twitter:url", content: "https://mirumi.tech" },
+        { property: "twitter:description", content: "みるみの技術ブログ" },
+        { name: "twitter:domain", content: "mirumi.tech" },
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:image", content: "/main-visual.png" },
       ],
-      "link": [
+      link: [
         { rel: "icon", href: "/site-icon.png" },
+        { rel: "canonical", href: "https://mirumi.tech" },
       ],
-      "style": [],
-      "script": [],
+      style: [],
+      script: [],
     },
   },
   autoImports: {
