27368540b5a246194da8818436386e6796a369ff
mirumi <info@milmemo.net>
2022-06-24 18:52:42 +0900

    add: all-tags page

--- src/pages/all-tags.vue
+++ src/pages/all-tags.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="all_tags_view">
-
+    <main class="single_column">
+      <header>
+        <h1>
+          すべてのタグ
+        </h1>
+      </header>
+      <div id="content">
+        <ul>
+          <li v-for="tag in tags" :key="tag">
+            <NuxtLink :to="`/tags/${tag}`">
+              {{ tag }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <footer>
+        <!-- (｡･ڡ･｡) -->
+      </footer>
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import { SITE_FULL_PATH, SITE_CREATED_AT } from "@/lib/defines"
+import { today } from "@/lib/utils"
+
 const router = useRouter()
 
+const tags = ref<Array<string>>()
+
+tags.value = ["JavaScript", "Python"]
 
 
 
@@ -14,10 +38,30 @@ const router = useRouter()
 
 
 
+useSetMeta({
+  title: "すべてのタグ",
+  description: "mirumi.tech のすべてのタグ一覧です。",
+  keywords: "みるめも,みるみ,blog,technology,programming,tags",
+  url: SITE_FULL_PATH + "/" + router.currentRoute.value.path,
+  createdAt: SITE_CREATED_AT,
+  updatedAt: today(),
+})
 </script>
 
 <style lang="scss" scoped>
 .all_tags_view {
-  
+  main {
+    #content {
+      ul {
+        li {
+          margin: 0;
+          line-height: 1.5;
+          a {
+            font-size: 0.88em;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
