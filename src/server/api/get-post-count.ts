496b0e14178db8a99b2fb64ee8c14da25b573188
mirumi <mail@mirumi.tech>
2022-07-12 18:03:20 +0900

    change: secrets with env var system (moreteta)

--- src/server/api/get-post-count.ts
+++ src/server/api/get-post-count.ts
@@ -1,6 +1,4 @@
-import dev from "@/secrets/secret.dev"
-import prd from "@/secrets/secret.prd"
-const secret = process.env.NODE_ENV === "development" ? dev : prd
+import secret from "@/secrets/export"
 
 export default defineEventHandler(async (event) => {
   const postCount = await $fetch<number>(`get-post-count`, {
