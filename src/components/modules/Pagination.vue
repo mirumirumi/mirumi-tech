7c70ca83b83d9d53e30ba54c4d2a83ea1a8d723e
mirumi <mail@mirumi.tech>
2022-07-13 15:42:07 +0900

    change: changed to use query parameters (works fine because to be `a`)

--- src/components/modules/Pagination.vue
+++ src/components/modules/Pagination.vue
@@ -2,38 +2,38 @@
   <div class="pagination">
     <Head>
       <Link rel="prev" v-if="1 < page" :href="prevlLink" />
-      <Link rel="next" v-if="page < pageCount" :href="nextlLink" />
+      <Link rel="next" v-if="page < pageCount" :href="movePage(page + 1)" />
     </Head>
 
-    <NuxtLink v-if="1 < page" :to="{ query: { page: page - 1 } }" class="arrow prev">
+    <a v-if="1 < page" :href="prevlLink" class="arrow prev">
       <PartsSvgIcon :icon="'arrow_left'" :color="'#727272'" />
-    </NuxtLink>
+    </a>
     <template v-if="page === 1">
       <div class="page_latest current">
         {{ 1 }}
       </div>
     </template>
     <template v-else>
-      <NuxtLink :to="{ query: { page: 1 } }" class="page_latest">
+      <a :href="latestLink" class="page_latest">
         {{ 1 }}
-      </NuxtLink>
+      </a>
     </template>
     <div v-if="LINKS_TO_SHOW <= page" class="tententen">
       <PartsSvgIcon :icon="'tententen'" :color="'#727272'" />
     </div>
 
     <template v-for="x in BEFORE_AND_AFTER.slice().reverse()">
-      <NuxtLink v-if="1 < page - x" :to="{ query: { page: page - x } }">
+      <a v-if="1 < page - x" :href="movePage(page - x)">
         {{ page - x }}
-      </NuxtLink>
+      </a>
     </template>
     <div v-if="page !== 1 && page !== pageCount" class="current">
       {{ page }}
     </div>      
     <template v-for="x in BEFORE_AND_AFTER">
-      <NuxtLink v-if="page + x < pageCount" :to="{ query: { page: page + x } }">
+      <a v-if="page + x < pageCount" :href="movePage(page + x)">
         {{ page + x }}
-      </NuxtLink>
+      </a>
     </template>
 
     <div v-if="page <= pageCount - (LINKS_TO_SHOW - 1)" class="tententen">
@@ -45,13 +45,13 @@
       </div>
     </template>
     <template v-else>
-      <NuxtLink :to="{ query: { page: pageCount } }" class="page_oldest">
+      <a :href="movePage(pageCount)" class="page_oldest">
         {{ pageCount }}
-      </NuxtLink>
+      </a>
     </template>
-    <NuxtLink v-if="page < pageCount" :to="{ query: { page: page + 1 } }" class="arrow next">
+    <a v-if="page < pageCount" :href="movePage(page + 1)" class="arrow next">
       <PartsSvgIcon :icon="'arrow_right'" :color="'#727272'" />
-    </NuxtLink>
+    </a>
   </div>
 </template>
 
@@ -71,15 +71,25 @@ const BEFORE_AND_AFTER = [...Array((LINKS_TO_SHOW - 1) / 2).keys()].map((x) => x
 const { data: postCount } = await useFetch<number>(`/get-post-count`)
 const pageCount = Math.ceil(postCount.value / PAGE_ITEMS)
 
+/**
+ * generate link urls
+ */
+const original = secret.ORIGIN + (router.currentRoute.value.path === "/" ? "" : router.currentRoute.value.path)
+
+const latestLink = computed(() => {
+  return original + `?page=1`
+})
+
 const prevlLink = computed(() => {
   if (p.page === 2)
-    return secret.ORIGIN + (router.currentRoute.value.path === "/" ? "" : router.currentRoute.value.path)
+    return original
   else
-    return secret.ORIGIN + (router.currentRoute.value.path === "/" ? "" : router.currentRoute.value.path) + `?page=${p.page - 1}`
+    return original + `?page=${p.page - 1}`
 })
-const nextlLink = computed(() => {
-  return secret.ORIGIN + (router.currentRoute.value.path === "/" ? "" : router.currentRoute.value.path) + `?page=${p.page + 1}`
-})
+
+const movePage = (page: number): string => {
+  return original + `?page=${page}`
+}
 </script>
 
 <style lang="scss" scoped>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,53 @@
 <template>
   <div class="index_view">
-    <ModulesTopIndexes :page="1" />
+    <main class="single_column">
+      <ul class="posts">
+        <li class="post" v-for="post in postLinks" :key="post.slag">
+          <NuxtLink :to="`/${post.slag}`">
+            <article>
+              <h2>
+                {{ post.title }}
+              </h2>
+              <div class="meta">
+                <div class="created_at">
+                  <PartsSvgIcon :icon="'edit'" :color="'#9e9e9e'" />
+                  <span>{{ post.created_at }}</span>
+                </div>
+                <div class="updated_at">
+                  <PartsSvgIcon :icon="'update'" :color="'#9e9e9e'" />
+                  <span>{{ post.updated_at }}</span>
+                </div>
+              </div>
+            </article>
+          </NuxtLink>
+        </li>
+      </ul>
+      <ModulesPagination :page="page" />
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import { SITE_FULL_PATH, SITE_CREATED_AT, PostLink } from "@/lib/defines"
+import { today } from "@/lib/utils"
+
+const router = useRouter()
+const page = ref(Number(router.currentRoute.value.query.page ?? 1))
+
+const { data: postLinks } = await useFetch<PostLink[]>(`/get-top-indexes`, {
+  params: {
+    page: page.value,
+  },
+})
+
+useSetMeta({
+  title: "mirumi.tech",
+  description: "みるみの技術ブログ",
+  keywords: "みるめも,みるみ,blog,technology,programming",
+  url: SITE_FULL_PATH,
+  createdAt: SITE_CREATED_AT,
+  updatedAt: today(),
+})
 </script>
 
 <style lang="scss" scoped>
