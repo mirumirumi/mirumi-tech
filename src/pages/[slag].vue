9444fa45fcb5d1079e57a5d4b4730e4050e59097
mirumi <mail@mirumi.tech>
2022-07-13 21:35:03 +0900

    fix: Prism.js have not highlighted at after 2nd page moving

--- src/pages/[slag].vue
+++ src/pages/[slag].vue
@@ -26,8 +26,7 @@
             </NuxtLink>
           </div>
         </header>
-        <div id="content" v-html="post.body">
-        </div>
+        <div id="content" v-html="post.body"></div>
         <footer>
           <!-- (｡･ڡ･｡) -->
         </footer>
@@ -39,7 +38,7 @@
 
 <script setup lang="ts">
 import { SITE_FULL_PATH, PostData } from "@/lib/defines"
-import { zeroPadding } from "@/lib/utils"
+import { delay, zeroPadding } from "@/lib/utils"
 import Prism from "prismjs"
 
 const router = useRouter()
@@ -51,7 +50,11 @@ const { data: post } = await useFetch<PostData>(`/get-post`, {
   },
 })
 
-onMounted(() => {
+onMounted(async () => {
+  do {
+    await delay(100)
+  } while (!document.getElementById("content"))
+
   Prism.manual = true
   Prism.highlightAll()
 })
@@ -60,17 +63,23 @@ useSetMeta({
   title: post.value.title,
   description: generateMetaDescription(post.value.body),
   keywords: post.value.tags.join(","),
-  url: SITE_FULL_PATH + "/" + slag,
+  url: SITE_FULL_PATH + "/" + slag.value,
   createdAt: post.value.created_at,
   updatedAt: post.value.updated_at,
 })
 
 const datetime = (datetime: string) => {
-  return `${datetime.slice(0, 4)}-${zeroPadding(Number(datetime.replace(/\d{4}\/(\d\d*)\/.*?$/gmi, "$1")), 2)}-${zeroPadding((Number(datetime.replace(/\d{4}\/\d\d*\/(.*?)$/gmi, "$1"))), 2)}`
+  return `${datetime.slice(0, 4)}-
+          ${zeroPadding(Number(datetime.replace(/\d{4}\/(\d\d*)\/.*?$/gmi, "$1")), 2)}-
+          ${zeroPadding((Number(datetime.replace(/\d{4}\/\d\d*\/(.*?)$/gmi, "$1"))), 2)}
+         `
 }
 
 function generateMetaDescription(html: string): string {
-  return html.replace(/(<.*?>|\n|^\n)/gmi, "").replace(/<h[234].*?>.*?<\/h[234]>/gmi, "").slice(0, 100)  // https://regex101.com/r/ke1Ymn/1
+  return html  // https://regex101.com/r/ke1Ymn/1
+    .replace(/(<.*?>|\n|^\n)/gmi, "")
+    .replace(/<h[234].*?>.*?<\/h[234]>/gmi, "")
+    .slice(0, 100)
 }
 </script>
 
