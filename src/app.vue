1b81ca4adc8a79f2ae04788878315c88cbbb710f
mirumi <mail@mirumi.tech>
2022-07-18 01:05:48 +0900

    chore: clean up (remove pinia, js-cookie)

--- src/app.vue
+++ src/app.vue
@@ -13,10 +13,6 @@
 // app.vue acts as the main component of your Nuxt application. Anything you add to it (JS and CSS) will be global and included in every page
 
 // to allow route transitions between pages, a page must have a single route element (HTML comments are also considered elements)
-
-// import { createPinia } from "pinia"
-
-// useNuxtApp().vueApp.use(createPinia())
 </script>
 
 <style lang="scss">
