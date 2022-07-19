2cb79ba5684809c6df5c51ce075739301acfffaf
mirumi <mail@mirumi.tech>
2022-07-19 17:11:25 +0900

    fix: scroll to top at page moving (not completely)

--- src/plugins/page-scroll.js
+++ src/plugins/page-scroll.js
@@ -2,7 +2,7 @@
 
 import { defineNuxtPlugin } from "#app"
 
-export default defineNuxtPlugin((nuxtApp) => {
+// export default defineNuxtPlugin((nuxtApp) => {
 //   nuxtApp.$router.options.scrollBehavior = (to, from, savedPosition) => {
 //     return new Promise((resolve) => {
 //       setTimeout(() => {
@@ -13,4 +13,13 @@ export default defineNuxtPlugin((nuxtApp) => {
 //       }, 0)
 //     })
 //   }
+// })
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook("page:start", async () => {  // `page:finish` does not return savedPosition
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    })
+  })
 })
