28411fc47a6d38363eb8208aea6690353ccfa94f
mirumi <mail@mirumi.tech>
2022-08-11 13:50:57 +0900

    add: Google AdSense (in testing)

--- src/components/modules/TheSidebar.vue
+++ src/components/modules/TheSidebar.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="sidebar_wrap">
-    <!-- <aside class="ad">
-      
-    </aside> -->
+    <aside class="ad" ref="ad">
+      <ClientOnly>
+        <ins class="adsbygoogle" style="display: inline-block; width: 300px; height: 250px;" data-ad-client="ca-pub-2873410957106428" data-ad-slot="8481345159" data-ad-format="rectangle"></ins>
+      </ClientOnly>
+    </aside>
     <aside class="toc">
       <ModulesToc :html="html" />
     </aside>
@@ -13,6 +15,16 @@
 defineProps<{
   html: string,
 }>()
+
+const ad = ref()
+
+onMounted(() => {
+  const script = document.createElement("script")
+  script.type = "text/javascript"
+  script.text = "(adsbygoogle = window.adsbygoogle || []).push({});"
+
+  ad.value.appendChild(script)
+})
 </script>
 
 <style lang="scss" scoped>
@@ -24,6 +36,9 @@ defineProps<{
   padding: 13px 13px;
   .ad {
     height: 250px !important;
+    ins.adsbygoogle[data-ad-status="unfilled"] {
+      display: none !important;
+    }
   }
   @include tablet {
     padding: 13px 0 13px 26px;
