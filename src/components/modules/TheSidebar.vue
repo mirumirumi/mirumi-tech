d5789a7368d732ea2262d4712fd39fd65221880f
mirumi <mail@mirumi.tech>
2022-10-18 18:16:58 +0900

    fix: use MutationObserver to prevent adsense from overriding `height: auto !important;` (#46)

--- src/components/modules/TheSidebar.vue
+++ src/components/modules/TheSidebar.vue
@@ -3,16 +3,18 @@
     <Head>
       <Script crossorigin="anonymous" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2873410957106428"></Script>
     </Head>
-    <ClientOnly>
-      <ins
-        class="adsbygoogle"
-        style="display: inline-block; width: 300px; height: 250px;"
-        data-ad-client="ca-pub-2873410957106428"
-        data-ad-slot="8481345159"
-        data-ad-format="rectangle">
-      </ins>
-    </ClientOnly>
-    <div ref="adspush"></div>
+    <aside class="ad" ref="adaside">
+      <ClientOnly>
+        <ins
+          class="adsbygoogle"
+          style="display: inline-block; width: 300px; height: 250px;"
+          data-ad-client="ca-pub-2873410957106428"
+          data-ad-slot="8481345159"
+          data-ad-format="rectangle">
+        </ins>
+      </ClientOnly>
+      <div ref="adspush"></div>
+    </aside>
     <aside class="toc">
       <ModulesToc :html="html" />
     </aside>
@@ -24,12 +26,23 @@ defineProps<{
   html: string,
 }>()
 
+const sidebar_wrap = ref()
 const adspush = ref()
+const targetStyle = ref("calc(100vh - 13px * 2)")
 
 onMounted(() => {
   const src = document.createElement("script") as HTMLScriptElement
   src.text = "(adsbygoogle = window.adsbygoogle || []).push({});"
   adspush.value.appendChild(src)  
+
+  // https://bit.ly/3MLZyCE
+  const observer = new MutationObserver((mutations, observer) => {
+    sidebar_wrap.value.style.height = targetStyle.value
+  })
+  observer.observe(sidebar_wrap.value, {
+    attributes: true,
+    attributeFilter: ["style"],
+  })
 })
 </script>
 
@@ -38,10 +51,13 @@ onMounted(() => {
   position: sticky;
   top: 6px;
   width: var(--width-sidebar);
-  height: calc(100vh - 13px * 2);
+  height: v-bind(targetStyle);
   padding: 13px 13px;
-  ins.adsbygoogle[data-ad-status="unfilled"] {
-    display: none !important;
+  .ad {
+    height: 250px;
+    ins.adsbygoogle[data-ad-status="unfilled"] {
+      display: none !important;
+    }
   }
   @include tablet {
     padding: 13px 0 13px 26px;
