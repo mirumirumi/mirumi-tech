923a6021179b7db75c3541ff6843be49f86bc96d
mirumi <mail@mirumi.tech>
2022-11-25 15:09:26 +0900

    change: use API GW directly without CloudFront

--- src/plugins/page-scroll.js
+++ src/plugins/page-scroll.js
@@ -2,19 +2,6 @@
 
 import { defineNuxtPlugin } from "#app"
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.$router.options.scrollBehavior = (to, from, savedPosition) => {
-//     return new Promise((resolve) => {
-//       setTimeout(() => {
-//         resolve({
-//           left: savedPosition?.left || 0,
-//           top: savedPosition?.top || 0,
-//         })
-//       }, 0)
-//     })
-//   }
-// })
-
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("page:start", async () => {  // `page:finish` does not return savedPosition
     window.scrollTo({
