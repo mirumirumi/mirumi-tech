fe61e5619a4cca9b55dfcd9b314fb8a93bc6c4a2
mirumi <mail@mirumi.tech>
2023-03-02 21:18:20 +0900

    fix: add trailing slash with canonical url

--- src/plugins/page-scroll.ts
+++ src/plugins/page-scroll.ts
@@ -1,7 +1,8 @@
 // // https://github.com/nuxt/framework/discussions/1661
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook("page:start", async () => {  // `page:finish` does not return savedPosition
+  // `page:finish` does not return savedPosition
+  nuxtApp.hook("page:start", async () => {
     window.scrollTo({
       top: 0,
       left: 0,
